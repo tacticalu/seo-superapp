763cfdc147be169a3dfc19d22686f4d8acb993e3
tacticalu <jewru@protonmail.com>
2025-05-03 13:36:24 -0400

    feat: register dashboard & admin index routes under AuthLayout

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,10 @@
-import { Route, createBrowserRouter, createRoutesFromElements, defer } from 'react-router-dom'
+// src/routes.tsx
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  defer,
+} from 'react-router-dom'
 
 import { AppLayout, PublicLayout, RootLayout } from './layouts'
 import { AuthLayout } from './layouts/auth-layout'
@@ -7,9 +13,9 @@ import Home from './pages/home'
 import UserDashboard from './pages/dashboard'
 import AdminDashboard from './pages/admin'
 
-// Fake “getUserData” loader
+// fake loader for user data (can ignore for now)
 const getUserData = () =>
-  new Promise(resolve =>
+  new Promise((resolve) =>
     setTimeout(() => {
       resolve(window.localStorage.getItem('user'))
     }, 3000)
@@ -17,21 +23,24 @@ const getUserData = () =>
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RootLayout />} loader={() => defer({ userPromise: getUserData() })}>
+    <Route
+      element={<RootLayout />}
+      loader={() => defer({ userPromise: getUserData() })}
+    >
+      {/* public pages */}
       <Route element={<PublicLayout />}>
         <Route path="/" element={<Home />} />
-      </Route>
-
-      <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
       </Route>
 
-      <Route path="/dashboard" element={<AppLayout />}>
-        <Route index element={<UserDashboard />} />
-      </Route>
-
-      <Route path="/admin" element={<AppLayout />}>
-        <Route index element={<AdminDashboard />} />
+      {/* authenticated area */}
+      <Route element={<AuthLayout />}>
+        <Route path="/dashboard" element={<AppLayout />}>
+          <Route index element={<UserDashboard />} />
+        </Route>
+        <Route path="/admin" element={<AppLayout />}>
+          <Route index element={<AdminDashboard />} />
+        </Route>
       </Route>
     </Route>
   )
