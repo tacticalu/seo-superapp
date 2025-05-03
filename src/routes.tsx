65a9c1b6051ee5212cba8637f742a4a30a3245d2
tacticalu <jewru@protonmail.com>
2025-05-03 14:23:45 -0400

    feat: register dashboard & admin routes under AuthLayout

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,11 @@
 // src/routes.tsx
-import { Route, createBrowserRouter, createRoutesFromElements, defer } from 'react-router-dom'
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  defer,
+} from 'react-router-dom'
+
 import { AppLayout, PublicLayout, RootLayout } from './layouts'
 import { AuthLayout } from './layouts/auth-layout'
 import { Login } from './pages/auth'
@@ -7,10 +13,13 @@ import Home from './pages/home'
 import { UserDashboard } from './pages/dashboard'
 import { AdminDashboard } from './pages/admin'
 
-// simulate fetching logged-in user data
+// Ideally this would be an API call to server to get logged in user data
 const getUserData = () => {
   return new Promise<string | null>((resolve) =>
-    setTimeout(() => resolve(window.localStorage.getItem('user')), 3000)
+    setTimeout(() => {
+      const user = window.localStorage.getItem('user')
+      resolve(user)
+    }, 3000)
   )
 }
 
@@ -20,17 +29,11 @@ export const router = createBrowserRouter(
       element={<RootLayout />}
       loader={() => defer({ userPromise: getUserData() })}
     >
-      {/* public pages */}
       <Route element={<PublicLayout />}>
         <Route path="/" element={<Home />} />
-      </Route>
-
-      {/* authentication */}
-      <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
       </Route>
 
-      {/* protected dashboards */}
       <Route element={<AuthLayout />}>
         <Route path="/dashboard" element={<AppLayout />}>
           <Route index element={<UserDashboard />} />
