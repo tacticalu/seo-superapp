cba2d8d70ef054bc5922d42227cbb1524084dfe2
tacticalu <jewru@protonmail.com>
2025-05-03 12:47:23 -0400

    Register dashboard routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,7 +4,8 @@ import { AppLayout, PublicLayout, RootLayout } from './layouts'
 import { AuthLayout } from './layouts/auth-layout'
 import { Login } from './pages/auth'
 import Home from './pages/home'
-import { UserDashboard } from './pages/users'
+import UserDashboard from './pages/dashboard'
+import AdminDashboard from './pages/admin'
 
 // Ideally this would be an API call to server to get logged in user data
 const getUserData = () => {
@@ -19,17 +20,24 @@ const getUserData = () => {
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route element={<RootLayout />} loader={() => defer({ userPromise: getUserData() })}>
+    <Route
+      element={<RootLayout />}
+      loader={() => defer({ userPromise: getUserData() })}
+    >
+      {/* Public pages */}
       <Route element={<PublicLayout />}>
         <Route path="/" element={<Home />} />
       </Route>
 
+      {/* Authentication pages */}
       <Route element={<AuthLayout />}>
         <Route path="/login" element={<Login />} />
       </Route>
 
-      <Route path="/dashboard" element={<AppLayout />}>
-        <Route path="overview" element={<UserDashboard />} />
+      {/* Protected app pages */}
+      <Route element={<AppLayout />}>
+        <Route path="/dashboard" element={<UserDashboard />} />
+        <Route path="/admin"     element={<AdminDashboard />} />
       </Route>
     </Route>
   )
