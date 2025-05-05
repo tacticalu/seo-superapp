eceb545ee19ee437a529bf2747b37da008e5e995
tacticalu <jewru@protonmail.com>
2025-05-05 19:27:49 -0400

    Add entry file for user dashboard in monorepo structure

---
+++ apps/user-dashboard/src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => {
+  return <div>SEO SuperApp – User Dashboard Placeholder</div>
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
