ce8e1250820eea93efdc2c2db569ea086236e882
tacticalu <jewru@protonmail.com>
2025-05-04 21:43:49 -0400

    fix: re-export all layout components using default exports

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,4 +1,5 @@
-export { default as AppLayout } from './app-layout';
-export { default as PublicLayout } from './public-layout';
-export { default as RootLayout } from './root-layout';
-export { default as AuthLayout } from './auth-layout';
+// src/layouts/index.ts
+export { default as PublicLayout } from './public-layout'
+export { default as RootLayout } from './root-layout'
+export { default as AppLayout } from './app-layout'
+export { default as AuthLayout } from './auth-layout'
