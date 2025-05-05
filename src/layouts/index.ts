aee2217d095770803db30b2bdbe8d803272b0b37
tacticalu <jewru@protonmail.com>
2025-05-04 21:35:58 -0400

    fix: export all layout components to resolve build failure

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,1 +1,4 @@
 export { default as AppLayout } from './app-layout';
+export { default as PublicLayout } from './public-layout';
+export { default as RootLayout } from './root-layout';
+export { default as AuthLayout } from './auth-layout';
