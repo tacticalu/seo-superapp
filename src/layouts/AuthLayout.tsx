2275edfcca33caf329623a8da7588495bb896607
tacticalu <jewru@protonmail.com>
2025-05-04 21:08:08 -0400

    Bypass auth checks temporarily for route access

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,10 +3,6 @@
 import { Outlet } from 'react-router-dom'
 
 export function AuthLayout() {
-  // TEMPORARY: Bypass auth redirect
-  return (
-    <div className="min-h-screen bg-gray-100 p-8 dark:bg-gray-900">
-      <Outlet />
-    </div>
-  )
+  // TEMPORARY DISABLE LOGIN CHECK
+  return <Outlet />
 }
